32480d8b3e79d3b40b7276029d9f8b4a7c332896
Leon Gell <lgell@lcs.on.ca>
2023-05-23 12:25:26 -0400

    Applied abstraction by adding a helper view to show article items.

--- AppleNews/Views/NewsItemView.swift
+++ AppleNews/Views/NewsItemView.swift
@@ -8,24 +8,31 @@
 import SwiftUI
 
 struct NewsItemView: View {
-    // 
+    // MARK: Stored Properties
+    let image: String
+    let source: String
+    let headline: String
+    let time: String
+    
+    
+    // MARK: Computed Properties
     var body: some View {
         VStack {
-            Image("Farming")
+            Image(image)
                 .resizable()
                 .scaledToFit()
             
-            Image("Globalmail")
+            Image(source)
                 .resizable()
                 .scaledToFit()
             
-            Text("hejwnainenien(write the actual text out)")
+            Text(headline)
                 .font(.title)
                 .fontWeight(.semibold)
             
             Divider()
             HStack {
-                Text("1h ago")
+                Text(time)
                 Spacer()
                 Image(systemName: "ellipsis")
             }
@@ -38,7 +45,10 @@ struct NewsItemView: View {
 
 struct NewsItemView_Previews: PreviewProvider {
     static var previews: some View {
-        NewsItemView()
+        NewsItemView(image: "Ejean",
+                     source: "Reuters",
+                     headline: "E. Jean Carroll seeks $10 million in damges from Trump over post-verdict statements",
+                     time: "1h ago")
             .preferredColorScheme(.dark)
     }
 }
--- AppleNews/Views/NewsListView.swift
+++ AppleNews/Views/NewsListView.swift
@@ -9,34 +9,26 @@ import SwiftUI
 
 struct NewsListView: View {
     var body: some View {
-        VStack {
-            Image("Farming")
-                .resizable()
-                .scaledToFit()
-            
-            Image("Globalmail")
-                .resizable()
-                .scaledToFit()
-            
-            Text("hejwnainenien(write the actual text out)")
-                .font(.title)
-                .fontWeight(.semibold)
-            
-            Divider()
-            HStack {
-                Text("1h ago")
-                Spacer()
-                Image(systemName: "ellipsis")
-            }
-            .foregroundColor(.secondary)
-            .padding(15)
+        ScrollView {
+            NewsItemView(image: "Ejean",
+                         source: "Reuters",
+                         headline: "E. Jean Carroll seeks $10 million in damges from Trump over post-verdict statements",
+                         time: "1h ago")
+            NewsItemView(image: "Farming",
+                         source: "Globalmail",
+                         headline: "Maritime farmers holfing breath as record-dry spring wrings region",
+                         time: "1h ago")
+            NewsItemView(image: "Sheep",
+                         source: "Guardian",
+                         headline: "New Zealand's ratio of sheep to humans at lowest point in 170 years",
+                         time: "1h ago")
         }
-        .background(Color("ArticleSlugBackground"))
     }
 }
 
 struct NewsListView_Previews: PreviewProvider {
     static var previews: some View {
         NewsListView()
+            .preferredColorScheme(.dark)
     }
 }
