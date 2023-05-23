00c6ead973ad0c24d123a18f36891f63e9e71286
Leon Gell <lgell@lcs.on.ca>
2023-05-23 11:38:11 -0400

    Initial Commit

---
+++ AppleNews/AppleNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleNewsApp.swift
+//  AppleNews
+//
+//  Created by Leon Gell on 2023-05-23.
+//
+
+import SwiftUI
+
+@main
+struct AppleNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
